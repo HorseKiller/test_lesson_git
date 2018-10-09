c54914c03f0d61756f05ba147f4afe5a9ecc5c8e
2Garin <d.a.konoval@gmail.com>
2018-10-09 21:38:13 +0300

    Наш первый комит

---
+++ src/test/java/com/dakon/lessongit/LessonGitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dakon.lessongit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LessonGitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
